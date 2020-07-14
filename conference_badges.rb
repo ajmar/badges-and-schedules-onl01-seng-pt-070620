6238d461355c4da9932d8c6f458cd2df4c6b2ce4
Alexander Marusin <amarusin18@gmail.com>
2020-07-14 23:38:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
-require pry
+
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
@@ -14,7 +14,6 @@ end
 
 def assign_rooms(attendees)
   array = []
-  binding.pry
   attendees.each.with_index(1) do |name|
   array << "Hello, #{name}! You'll be assigned to room #{i}!"
   end
