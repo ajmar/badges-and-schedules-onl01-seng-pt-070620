0cb263b3c2795cba37aa17186e02d8530af905b8
Alexander Marusin <amarusin18@gmail.com>
2020-07-15 00:21:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,8 +21,8 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  badges = batch_badge_creator
-  rooms = assign_rooms
+  badges = batch_badge_creator(attendees)
+  rooms = assign_rooms(attendees)
   array1 = []
   attendees.each do |badges, rooms|
   array1 << "#{badges}, #{rooms}"
