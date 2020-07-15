ccafb8f37c097788839911cdf64317b4b0cee577
Alexander Marusin <amarusin18@gmail.com>
2020-07-15 00:27:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,7 @@ end
 def printer(sticker)
   badges = batch_badge_creator(attendees)
   rooms = assign_rooms(attendees)
-  sticker.each do |badges|
-   puts "Hello, my name is #{badges}."
+  sticker.each do |badges, rooms|
+   puts "Hello, #{badges}! You'll be assigned to room #{rooms}!"
   end
 end
