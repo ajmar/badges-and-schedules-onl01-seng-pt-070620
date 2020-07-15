e4ed874f589de2c5eed78303196e835a0539eb69
Alexander Marusin <amarusin18@gmail.com>
2020-07-15 00:16:38 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -22,7 +22,7 @@ end
 
 def printer(attendees)
   array = []
-  attendees.each do
+  attendees.each do |batch_badge_creator, assign_rooms|
   array << "#{batch_badge_creator}, #{assign_rooms}"
   end
   puts array
